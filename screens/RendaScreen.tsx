c0127246e917c9121539660c4df1e967d55a9354
Matheus Baptista Rodrigues <matheusim27@hotmail.com>
2021-05-27 22:59:08 -0300

    Tentativa do datepicker

--- screens/RendaScreen.tsx
+++ screens/RendaScreen.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 import { RootStackParamList } from '../types';
+import { Button } from '../components/Button';
+
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
+
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function RendaScreen({
   navigation,
 }: StackScreenProps<RootStackParamList, 'Renda'>) {
+  const [date, setDate] = useState();
+
   return (
-    <View style={styles.header}>
-      <View>
-        <Text style={styles.title}>Cadastro de Renda</Text>
+    <View>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>Cadastro de Renda</Text>
+        </View>
+        <Image
+          style={styles.logo}
+          source={require('../assets/images/Logo.png')}
+        />
       </View>
-      <Image
-        style={styles.logo}
-        source={require('../assets/images/Logo.png')}
-      />
+
+      <View style={styles.input}>
+        <TextInput />
+        <Text></Text>
+
+        <View></View>
+      </View>
+
+      <View style={styles.footer}>
+        <Button
+          title="Cadastrar Renda"
+          onPress={() => navigation.replace('Root')}
+        />
+      </View>
     </View>
   );
 }
@@ -32,7 +54,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: '10%',
     // paddingHorizontal: 10,
-
     color: colors.header,
     fontFamily: fonts.heading,
     flexDirection: 'row',
@@ -44,10 +65,17 @@ const styles = StyleSheet.create({
     color: colors.header,
     marginTop: 7,
   },
-  subTitle: {},
   logo: {
     justifyContent: 'flex-end',
     height: 60,
     width: 60,
+  },
+  input: {
+    flex: 1,
+    marginTop: '10%',
+    marginBottom: '100%',
+  },
+  footer: {
+    paddingHorizontal: 37,
   },
 });
