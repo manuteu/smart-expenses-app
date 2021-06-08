ad259d87dcbc7f5db1cb4623000648ceddb3a1c0
diegodfs05 <diegofernandes.dfs@live.com>
2021-06-08 19:49:21 -0300

    correcao-picker

--- src/screens/DespesaScreen.tsx
+++ src/screens/DespesaScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, View, Image } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
@@ -11,35 +10,16 @@ import { ScrollView, TextInput, TouchableWithoutFeedback } from 'react-native-ge
 export default function Despesa() {
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <KeyboardAvoidingView style={styles.container}>
-          <View style={styles.header}>
-            <View>
-              <Text style={styles.title}>Cadastro de despesa</Text>
-            </View>
-            <Image style={styles.logo} source={require('../../assets/images/Logo.png')}/>
-          </View>
-            <View>
-              <Text style={styles.textBelowInput}>Tipo de despesa</Text>
-              <Picker
-                selectedValue={'Poupança'}
-                
-                >
-                <Picker.Item>Água</Picker.Item>
-                <Picker.Item>Energia</Picker.Item>
-                <Picker.Item>Aluguel</Picker.Item>
-                <Picker.Item>Internet</Picker.Item>
-                <Picker.Item>Telefone</Picker.Item>
-                <Picker.Item>Alimentação</Picker.Item>
-                <Picker.Item>Lazer</Picker.Item>
-                <Picker.Item>Poupança</Picker.Item>
-                <Picker.Item>Educação</Picker.Item>
-                <Picker.Item>Transporte</Picker.Item>
-                <Picker.Item>Vestuário</Picker.Item>
-              </Picker>
-            </View>
-        </KeyboardAvoidingView>
-      </TouchableWithoutFeedback>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>Cadastro de despesa</Text>
+        </View>
+        <Image style={styles.logo} source={require('../../assets/images/Logo.png')} />
+      </View>
+      <View>
+        <Text style={styles.textBelowInput}>Tipo de despesa</Text>
+
+      </View>
     </SafeAreaView>
   );
 }
